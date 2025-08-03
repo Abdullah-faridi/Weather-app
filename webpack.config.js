54add512cc4685181e3a54e5de1bb8d23a0f29ae
Abdullah Faridi <193290322+Abdullah-faridi@users.noreply.github.com>
2025-08-03 18:09:45 +0000

    Fix: CSS not loading on GitHub Pages (added style-loader)

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/Weather-app/",
     clean: true,
     assetModuleFilename: "images/[hash][ext][query]",
   },
